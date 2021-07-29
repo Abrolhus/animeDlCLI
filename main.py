7936019f862518412afa76190ad483043df07b5e
Samuel Abreu <samuelapp@outlook.com>
2021-07-29 11:03:55 -0300

    feat: multi argument anime name, colored errors
    Dont need to use strings in the input, eg: $ this_program 'Shingeki no Kyojin' =>  this_program Shingeki no Kyojin

--- main.py
+++ main.py
@@ -8,70 +8,59 @@ import requests
 import logging
 import subprocess
 import tempfile
+import mpv
 
 from anime_downloader.sites import get_anime_class
 import util
 
+def logHandler(loglevel, component, message):
+    # TODO: use style config for this loghandler. Instead of hardcoding hera
+    if loglevel == 'error':
+        colored_loglevel = click.style(loglevel, fg='red')
+    elif loglevel == 'warn':
+        colored_loglevel = click.style(loglevel, fg='yellow')
+    click.echo('[{}] {}: {}'.format(colored_loglevel, component, message))
 
 @click.command()
-@click.argument('name')
+@click.argument('name', nargs=-1)
 @click.option('-e', '--ep', default=1, help='episode no.')
 @click.option('--provider', default='twist.moe', help='site to get episodes from')
 @click.option('--autoplay', is_flag=True, help='autoplays next episode')
-def hello(name, ep, provider, autoplay):
+@click.option('--log-level', '--ll', default='error', type=click.Choice(["no", "fatal", "error", "warn", "info", "v", "debug", "trace"], case_sensitive=False))
+def hello(name, ep, provider, autoplay, log_level):
+    strName = " ".join(name)
     Anime = get_anime_class(provider)
     player = 'mpv'
-    searchResults = Anime.search(name)
-    # click.echo(searchResults)
-    anime = Anime(searchResults[0].url)
-    print(anime)
 
+    searchResults = Anime.search(strName)
+    anime = Anime(searchResults[0].url)
+    click.echo('anime: {}'.format(anime)
     episode = anime[ep-1]
-    episode2 = anime[ep]
-    click.echo(episode.source().stream_url)
-    #click.echo(episode2.source().stream_url)
-    click.echo(episode.source().referer)
-    # util.play_episode(episode, player=player, title=f'{anime.title} - Episode {episode.ep_no}')
-    # title=f'{anime.title} - Episode {episode.ep_no}'
-    # title2=f'{anime.title} - Episode {episode2.ep_no}'
-        # p = subprocess.Popen([
-            # player,
-            # '--title={}'.format(title),
-            # '--referrer="{}"'.format(episode.source().referer),
-            # episode.source().stream_url,
-            # '--title={}'.format(title2),
-            # '--referrer="{}"'.format(episode2.source().referer),
-            # episode2.source().stream_url
-        # ])
-    tfile = tempfile.NamedTemporaryFile(mode='a+', suffix='.m3u8')
-    mpvArgs = [player, '--referrer={}'.format('https://twist.moe/'), '--playlist']
+    #click.echo('stream URL: {}'.format(episode.source().stream_url))
+    #click.echo('referrer: {}'.format(episode.source().referer))
     if player == 'mpv':
-        util.makePlaylist(anime[0:1], tfile)
-        # for epi in anime:
-        # title = f'{anime.title} - Episode {epi.ep_no}'
-        # mpvArgs += ['--title={}'.format(title),
-        # 'ffmpeg://{}'.format(epi.source().stream_url)]
-        # click.echo("uai")
-        print(tfile.name)
-        mpvArgs.append(tfile.name)
-        print(mpvArgs)
-        tfile.seek(0)
-        print(tfile.read())
-        #mpvArgs.append('{0} >/dev/null 2>&1 &')
-        #subprocess.Popen("nohup usr/local/bin/otherscript.pl {0} >/dev/null 2>&1 &", shell=True)
-        print(''.join(mpvArgs))
-        util.addAnimesToPlaylist(anime[1:], tfile)
-        p = subprocess.Popen(mpvArgs, stdin=None, stdout=None, stderr=None)
-        print("humm")
-        print(anime[0:1])
-        print(anime[1:])
-        print("uaaaaaaaaaaaaaaaaaaaaaaaa");
-        tfile.seek(0)
-        print(tfile.read())
-        p.wait()
+        player = mpv.MPV(input_default_bindings=True, input_vo_keyboard=True, osc=True, log_handler=logHandler, loglevel=log_level)
+        title = f'{anime.title} - Episode {episode.ep_no}'
+        url = 'ffmpeg://{}'.format(episode.source().stream_url)
+        referrer = episode.source().referer
+        player.loadfile(url, "append-play", title=title, referrer=referrer)
+        #player.loadfile("../testeMpvJsonIpc/v2.mp4")
+
+        for episode in anime[1:]:
+            title = f'{anime.title} - Episode {episode.ep_no}'
+            url = 'ffmpeg://{}'.format(episode.source().stream_url)
+            referrer = episode.source().referer
+            player.playlist_append(url, title=title, referrer=referrer)
+        player.wait_for_property('idle-active')
     else:
-        p = subprocess.Popen([player, episode.source().stream_url])
-        p.wait()
+        pass
+        #p = subprocess.Popen([player, episode.source().stream_url])
+        #p.wait()
 
 if __name__ == "__main__":
     hello()
+
+style = {
+    errColor : ('red', None),
+    warnColor : ('yellow', None)
+}
